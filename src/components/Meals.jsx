d6e6f0e0812c8c560c17a102b6528cb6ddbe7d49
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-12-05 23:35:48 +0530

    finish

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,47 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { fetchAvailableMeals } from "../http.js";
 import MealItem from './MealItem.jsx';
+import useHttp from '../hooks/useHttp.js';
+import Error from './Error.jsx';
+
+const requestConfig = {};
 
 export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-    const [isFetching, setIsFetching] = useState(false);
-    const [meals, setMeals] = useState([]);
-    const [error, setError] = useState();
+  if (isLoading) {
+    return <p className='center'>Fetching meals...</p>;
+  }
 
-    // console.log(meals);
-    useEffect(() => {
-
-        setIsFetching(true);
-        async function fetchMeals() {
-            try {
-              const response = await fetch('http://localhost:3000/meals');
-              // console.log(response)
-              const resData = await response.json();
-              setMeals(resData);
-
-              // const availableMeals = await fetchAvailableMeals();
-              //   // console.log(availableMeals);
-              //   setMeals(availableMeals);
-
-        setIsFetching(true);
-
-            } catch (error) {
-                setError({message: error.message || 'could not fetch meals'});
-                
-            }
-        setIsFetching(true);
-
-        }
-        fetchMeals();
-    }, []);
+  if (error) {
+    return <Error title='Falied to get meals.' message={error} />
+  }
+  // if (!data) {
+  //   return <p>No meals found.</p>
+  // }
 
   return (
-    <div>
-      <ul id='meals'>
-        {meals.map((meal) => (
-        <MealItem key={meal.id} meal={meal}/>
-        ))}
-      </ul>
-    </div>
-  )
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 }
